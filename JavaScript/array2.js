621c0ac11f7c42b01939ef3527a985c1e764b737
Ajay Chauhan <Chauhanajay2801@gmail.com>
2022-12-22 12:24:22 +0530

    positions css article

---
+++ JavaScript/array2.js
@@ -0,0 +1,14 @@
+// let name = ["Ajay","Hitesh","Himanshu","Kapil","Pritam"];
+// console.log(name);
+// console.log(name.length);
+// console.log(name[name.length - 1]);
+
+// name[4] = "Vineet";
+// console.log(name)
+
+
+// let names = new Array("Name1","Name2"); //Also a way to declare array
+// console.log(names);
+
+let name = ["Ajay","Hitesh","Himanshu","Kapil","Pritam"];
+name.splicedrtt
