e452e9c49c58dc3cda9de432e0bea96dca9bb9ca
Ajay Chauhan <Chauhanajay2801@gmail.com>
2022-12-24 13:45:21 +0530

    array and fuction practice

---
+++ JavaScript/Arraypractice.js
@@ -0,0 +1,168 @@
+// let indiancar = ["marutisuzuki","tata","mahindra"]
+// mixedcar = foreigncar.concat(indiancar);
+
+// let cars = ["volswagen","bmw","jaguar","landrover","volswagen","jaguar","landrover","volswagen"];
+// // console.log(cars.("volswagen"));
+
+// // let numbers = [1,2,3,4,5,6,7,8];
+// // numbers.copyWithin(4,0,3)
+// // console.log(numbers);
+
+
+// // let array1 = ['a', 'b', 'c'];
+
+// // let iterator1 = array1.entries();
+
+// // for (let i=0; i<=2; i++){
+// // console.log(iterator1.next().value);
+// // }
+
+
+// let even = [2,4,6,40,10,80];
+
+
+
+// function checkeven(even) {
+//   return even%2==0;
+// }
+
+// console.log(even.every(checkeven));
+
+// const isBelowThreshold = (currentValue) => currentValue < 40;
+
+// const array1 = [1, 30, 39, 29, 10, 13];
+
+// console.log(array1.every(isBelowThreshold));
+
+
+// const array1 = [1, 30, 39, 29, 10, 13];
+// const result = array1.filter(array1 => array1 %2 ==0);
+
+// console.log(result);
+// // expected output: Array ["exuberant", "destruction", "present"]
+
+// let array = [5, 12, 8, 130, 44];
+
+// let found = array.find(element => element > 10);
+
+// console.log(found);
+// // expected output: 12
+
+// const array1 = ['a', 'b', 'c'];
+
+// array1.forEach(element => console.log(element));
+
+// let cars = ["volswagen","bmw","jaguar","landrover","volswagen","jaguar","landrover","volswagen"];
+// cars.forEach(element => console.log(element));
+
+// console.log(Array.from("Volsvagen"));
+// // expected output: Array ["f", "o", "o"]
+
+// let cars = ["volswagen","bmw","jaguar","landrover","volswagen","jaguar","landrover","volswagen"];
+// let index = 2;
+// console.log(cars.at(index));
+
+
+// let cars = ["volswagen","bmw","jaguar","landrover"];
+
+// const isLargeNumber = (element) => element.length < 4;
+
+// console.log(cars.findLast(isLargeNumber));
+
+
+// let cars = ["volswagen","bmw","jaguar","landrover"];
+
+// const isLargeNumber = (element) => element.length < 4;
+
+// console.log(cars.findLastIndex(isLargeNumber));
+
+
+
+// let cars = ["volswagen","bmw","jaguar","landrover"];
+
+// const carrss = cars.values();
+
+// for (const value of carrss){
+//     console.log(value);
+// }
+
+
+// let cars = ["volswagen","bmw","jaguar","landrover"];
+// console.log(cars.toString());
+// console.log(Array.isArray(cars));
+
+
+// let nums = [1, 2, 3, 4, 5];
+
+
+// let checkeven = (element) => element % 2 === 0;
+
+// console.log(nums.some(checkeven));
+
+
+// let cars = ["volswagen","bmw","jaguar","landrover",["Yamaha","Bajaj"]];
+
+// console.log(cars.flat()); //taking default value
+
+// let carss = ["volswagen","bmw","jaguar","landrover",[[["Yamaha","Bajaj"]]]];
+
+// console.log(carss.flat(2)); //remove two level of nesting 
+
+// let numms = [1, 2, [3], [4], 6, []];
+// let flattened = (numms.flatMap(num => [num*2]));
+// console.log(flattened);
+
+// let numms = [ 5,6,7,8 ]; 	 	
+// // array.keys() method is called 	
+// let iterator = numms.keys(); 	 	
+// // printing index array using the iterator 	
+// for (let key of iterator) { 		
+// console.log(key); 	}
+
+// const numbers = [1, 2, 3, 4];
+// const newArr = numbers.map(Function)
+// console.log(newArr)
+
+// function Function(num) {
+//   return num * 2;
+// }
+
+
+
+// console.log(Array.of(11, 21, 33));
+// console.log(Array.of(1,2,3,4));
+// console.log(Array.of("Ram","Geeta"));
+// console.log(Array.of('geeksforgeeks'));
+// console.log(Array.of(2,3,4,'Sheeta'));
+
+// let arr1 = [1,2,3,4,5,6,7,8,9]
+// let newarr = arr1.reduce((a,b) =>{
+ 
+//         return a+b;
+// })
+
+// console.log(newarr);
+
+// let arr1 = [1,2,3,4,5,6,7,8,9]
+// let newarr = arr1.reduceRight((a,b) =>{
+ 
+//         return a-b;
+// })
+// console.log(newarr);
+
+// var carName = "Maruti";
+// var carName ;
+// console.log(carName);
+
+// var _a = 2;
+// console.log(_a);
+
+
+// let a;
+// console.log(a);
+
+
+{
+const a =10;
+}
+console.log(a);
---
+++ JavaScript/functionpractice.js
@@ -0,0 +1,45 @@
+// function sum(val1,val2){
+//     let result = val1+val2;
+//     return result;
+// }
+
+// function sumarray(ar){
+//     let a = 0;
+//     for(let i =0; i<ar.length; i++){
+//         a = a+ar[i];
+//     }
+//     return a;
+// }
+
+
+
+// summ = console.log(sum(1,2));
+// c =[1,2,3,4,5,6,7,8,9]
+// summm = console.log(sumarray(c));
+
+
+// Traditional anonymous function
+(function (a, b) {
+    return a + b + 100;
+  });
+  
+// // Arrow function
+// let summ = (a, b) => a + b + 100;
+  
+//  aa = console.log(summ(10,20));
+  
+
+
+// const func = (x) => x * x;
+// // concise body syntax, implied "return"
+
+// const func2 = (x, y) => {
+//    return x + y;
+// };
+// // with block body, explicit "return" needed
+
+// aa = console.log(func(10));
+// ab = console.log(func2(10,20));
+
+
+
