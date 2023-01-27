724a3027fb10f519a63f61362d0571d1d8465b8f
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-01-27 17:54:18 +0530

    few more component

--- React/newsapp/src/components/News.js
+++ React/newsapp/src/components/News.js
@@ -4,9 +4,21 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a new component
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>NewsKing - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+        <NewsItem title ="mtTitle" description= "mydesc" imageUrl = "https://ichef.bbci.co.uk/news/1024/branded_news/04BD/production/_128431210_screenshot2023-01-26at6.38.50pm.png" newsUrl="todo"/>
+        </div>
+          <div className="col-md-4">
+        <NewsItem title ="mtTitle" description= "mydesc"/>
+        </div>
+          <div className="col-md-4">
+        <NewsItem title ="mtTitle" description= "mydesc"/>
+        </div>
+     
+        
+        </div>
       </div>
     )
   }
