f3f45768b91db50da782263baffac2f41242c860
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-02-09 13:07:51 +0530

    dom manipulation

--- JavaScript/browser js/bom.js
+++
@@ -1,3 +0,0 @@
-console.log(window)
-console.log(document.body)
-document.body.style.background = "blue"
--- JavaScript/browser js/consoleinfo.js
+++
@@ -1,9 +0,0 @@
-console.log("helloo");
-let a = 2;
-let b =3;
-console.log(a+b);
-
-console.log(console);
-console.info("This is important bro")
-console.warn("Dont drink and drive")
-console.assert(3>6)
