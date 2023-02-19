b9aadf3518758af712558839db71757aec266dd3
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-02-19 15:24:51 +0530

    api added

--- React/newsapp/src/components/NewsItem.js
+++ React/newsapp/src/components/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
     return (
       <div className = "my-3">
         <div className="card" style={{width: "18rem"}}>
-         <img src={imageUrl} className="card-img-top" alt="..."/>
+         <img src={!imageUrl?"https://deadline.com/wp-content/uploads/2023/02/Deadline-Contenders-Film-Nominees-2022-Causeway-1920-x-1080.jpg?w=1024":imageUrl} className="card-img-top" alt="..."/>
          <div className="card-body">
           <h5 className="card-title">{title}</h5>
          <p className="card-text">{description}</p>
