b58efa7cbef6acdc272753d5198afd19b88508f2
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-02-23 08:33:46 +0530

    new project for learning

---
+++ React/learning/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Greer from './components/Greer';
+
+function App() {
+  return (
+    <div className="App">
+      <Greer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/learning/src/components/Greer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// function Greer(){
+//     return <h1>Hello bro this is the first component</h1>
+// }
+
+const Greer = () => <h1>Hey bro</h1>
+export default Greer
