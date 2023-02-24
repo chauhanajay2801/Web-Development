5ce94e07635943b70b39c402d6008265dc0c9b8f
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-02-24 15:54:50 +0530

    applyimg css

--- React/learning/src/App.js
+++ React/learning/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import NameList from './components/NameList';
-
+// import Inline from './components/Inline';
+// import Stylesheet from './components/Stylesheet';
+// import NameList from './components/NameList';
+import styles from './appStyles.module.css'
+import './appStyles.css'
 
 function App() {
   return (
     <div className="App">
-     <NameList/>
-    </div>
+     {/* <NameList/> */}
+     {/* <Stylesheet primary={true}/> */}
+     {/* <Inline /> */}
+  <h1 className='error'>Error</h1>
+  <h1 className={styles.success}>Success</h1>
+     </div>
   );
   }
 
