c12429f18ad83fd9583c78b18270abe33d18ef60
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-02-28 12:16:59 +0530

    Jsx Fragments

--- React/learning/src/App.js
+++ React/learning/src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
-import LifeA from './components/LifeA';
-// import Form from './components/Form';
-// import Inline from './components/Inline';
-// import Stylesheet from './components/Stylesheet';
-// import NameList from './components/NameList';
-// import styles from './appStyles.module.css'
-// import './appStyles.css'
+// import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
+
 
 function App() {
   return (
     <div className="App">
-     {/* <NameList/> */}
-     {/* <Stylesheet primary={true}/> */}
-     {/* <Inline /> */}
-  {/* <h1 className='error'>Error</h1>
-  <h1 className={styles.success}>Success</h1> */}
-  {/* <Form/> */}
-   <LifeA/> 
+      {/* <FragmentDemo/> */}
+      <Table/>
      </div>
   );
   }
