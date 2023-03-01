d17b375b318f0bb23f03114b355de7041a3a911b
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-03-01 10:35:25 +0530

    Counter functionality

--- React/learning/src/App.js
+++ React/learning/src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react'
-import ErrorBoundary from './components/ErrorBoundary'
-import Hero from './components/Hero'
-
+import ClickCounter from './components/ClickCounter'
+import HoverCounter from './components/HoverCounter'
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <ErrorBoundary>
-       <Hero heroName={"Thor"}/>
-       </ErrorBoundary>
-        <ErrorBoundary>
-       <Hero heroName={"Hulk"}/>
-       </ErrorBoundary>
-        <ErrorBoundary>
-       <Hero heroName={"Joker"}/>
-       </ErrorBoundary>
+       <ClickCounter/>
+       <HoverCounter/>
       </div>
     )
   }
---
+++ React/learning/src/components/ClickCounter.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class ClickCounter extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         count:0
+      }
+    }
+
+    incrementCount = () =>{
+        this.setState(prevState => {
+            return {count:prevState.count + 1 }
+        })
+    }
+
+  render() {
+      
+        const { count } = this.state
+        return <button onClick={this.incrementCount}>click {count} times</button>
+      
+  }
+}
+
+export default ClickCounter
