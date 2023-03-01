6e6b6a513f0c2e1256bfbd9782edabf778c5f2b9
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-03-01 10:07:19 +0530

    Error Boundarues

--- React/learning/src/App.js
+++ React/learning/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
-import PortalDemo from './components/PortalDemo'
+import ErrorBoundary from './components/ErrorBoundary'
+import Hero from './components/Hero'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <PortalDemo/>
-
+        <ErrorBoundary>
+       <Hero heroName={"Thor"}/>
+       </ErrorBoundary>
+        <ErrorBoundary>
+       <Hero heroName={"Hulk"}/>
+       </ErrorBoundary>
+        <ErrorBoundary>
+       <Hero heroName={"Joker"}/>
+       </ErrorBoundary>
       </div>
     )
   }
