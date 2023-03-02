89e0809dbb9074497ed35a6e4b902f89d4fa35cc
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-03-02 13:34:05 +0530

    usestate counter

--- React/hooks-learning/src/App.js
+++ React/hooks-learning/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
 
 function App() {
   return (
     <div className="App">
-      <ClassCounter/>
+      <HookCounter/>
     </div>
   );
 }
