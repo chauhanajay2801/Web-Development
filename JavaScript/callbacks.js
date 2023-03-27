7b6ba186f9b52ffcee16df47657685b76aa06df4
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-03-27 10:39:37 +0530

    set timeout

--- JavaScript/callbacks.js
+++ JavaScript/callbacks.js
@@ -29,13 +29,34 @@
 // call(101,show);
 
 
-function ressult(a) {
-  console.log("The sum of digit" + a);
+// function ressult(a) {
+//   console.log("The sum of digit" + a);
+// }
+
+// function myCalculator(num1, num2,callback) {
+//   let a = num1 + num2;
+//   callback(a);
+// }
+
+// myCalculator(10,20,ressult);
+
+// const arr = ["hey","ji","hello","namaste","ramram ji"];
+// arr.forEach((val) => {
+  // console.log(val);
+// });
+
+//SetTimeOut
+
+// setTimeout(()=>{
+  // console.log("Hello this is the default timeout")},3000);
+
+
+function ajay(){
+  console.log("new function")
 }
 
-function myCalculator(num1, num2,callback) {
-  let a = num1 + num2;
-  callback(a);
-}
+setTimeout(ajay,4000);
 
-myCalculator(10,20,ressult);
+
+// setInterval
+
