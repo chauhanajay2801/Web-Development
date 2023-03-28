d6fc87251dd1063f5c1c3dd8c5c98b25b1cc7a7e
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-03-28 14:39:55 +0530

    spread and rest

--- JavaScript/spread_rest.js
+++ JavaScript/spread_rest.js
@@ -8,4 +8,29 @@ function someone(x,y,z,q,m) {
 
 let vari = [5,7,5,4,2];
 
-console.log(someone(...vari)); //spread
+console.log(someone(...vari)); //spread and the data is divided it is called as spread
+
+
+// Rest
+
+function sum(...args){  //rest operator
+    console.log(args); // it is making the array and the spread is breaking the array
+    let sum =0;
+    for (const x of args){
+        sum = sum+x;
+    }
+    return sum;
+}
+
+console.log(sum(1,2,3,4,5,6,7,8,9,10))
+
+
+
+
+
+
+
+
+
+
+
