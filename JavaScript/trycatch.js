5c3caa08d502d77b8cc824188e9ba3a86e5c421c
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-05 14:04:18 +0530

    new updates

--- JavaScript/trycatch.js
+++ JavaScript/trycatch.js
@@ -2,12 +2,21 @@
 
 //syntax
 
-try{
+// try{
     // logical error
-    console.log(Ajay);
-    
+    // console.log(Ajay);
+    // }
+// catch(err){
+    //error handling message
+    // console.log(err.name);
+//    }
+
+try{
+    let firstname = "Anurag";
+    console.log(firstname + "" + lastname);
 }
 catch(err){
-    //error handling message
-
+    console.log(err.name , "variable name missing");
 }
+
+
