19470de3fc81fce34de582f62ecf2f05dbd0ed09
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-06 12:24:31 +0530

    first assignment first task completed

---
+++ DOM Projects/01,02,03_DOM Project/firstAssignmentImage/first_assignment.js
@@ -0,0 +1,6 @@
+/**********************************TASK 1*******************************************/
+//Adding li to ul
+const ul = document.getElementsByTagName("ul")[0];
+const hireMe = document.createElement("li");
+hireMe.innerText = "Hire Me";
+ul.appendChild(hireMe);
