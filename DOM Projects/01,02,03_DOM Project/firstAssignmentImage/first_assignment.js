2769a7184c83c4d7aeae8305e03f957d05c85536
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-06 12:36:36 +0530

    first assignment task two completed

--- DOM Projects/01,02,03_DOM Project/firstAssignmentImage/first_assignment.js
+++ DOM Projects/01,02,03_DOM Project/firstAssignmentImage/first_assignment.js
@@ -4,3 +4,8 @@ const ul = document.getElementsByTagName("ul")[0];
 const hireMe = document.createElement("li");
 hireMe.innerText = "Hire Me";
 ul.appendChild(hireMe);
+
+/**********************************TASK 2*******************************************/
+//Adding search my prjects in the search bar
+const sear = document.querySelector(".search-field > input");
+sear.placeholder = "Search My Project";
