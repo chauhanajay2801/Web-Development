fc83e4058f6f161d6c629bc46f8ab5bee58f51d8
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-09 21:24:13 +0530

    total 18 done

---
+++ Js Assignment Q-A/Q 16/Season.js
@@ -0,0 +1,52 @@
+// Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+//     - September, October or November, the season is Autumn.
+//     - December, January or February, the season is Winter.
+//     - March, April or May, the season is Spring
+//     - June, July or August, the season is Summer
+
+let str1 = prompt("Enter the month");
+switch (str1) {
+  case "September":
+    alert(" The season is Autumn");
+    break;
+  case "October" :
+    alert(" The season is Autumn");
+    break;
+  case "November":
+    alert(" The season is Autumn");
+    break;
+
+  case "December":
+    alert(" The season is Winter");
+    break;
+  case "February":
+    alert(" The season is Winter");
+    break;
+  case "January":
+    alert(" The season is Winter");
+    break;
+
+  case "April":
+    alert(" The season is Spring");
+    break;
+  case "May":
+    alert(" The season is Spring");
+    break;
+  case "March":
+    alert(" The season is Spring");
+    break;
+
+  case "June":
+    alert(" The season is Summer");
+    break;
+  case "August":
+    alert(" The season is Summer");
+    break;
+  case "July":
+    alert(" The season is Summer");
+    break;
+  
+    
+}
+
+
---
+++ Js Assignment Q-A/Q 30/Calculator.js
@@ -0,0 +1,29 @@
+// Create a simple calculator program in JavaScript which can perform the addition, substraction, multiplication and division on given numbers.
+// User Input
+let n1 = 12;
+let n2 = 2;
+let operator = "/";
+
+    if (operator == "+") {
+        output = n1 + n2;
+        console.log(`The sum of the above two digits are ${output}`)
+    }
+    
+    else if (operator=="-"){
+        output = n1-n2;
+        console.log(`The subtraction of the above two digits are ${output}`)
+    }
+    else if (operator=="*"){
+        output = n1*n2;
+        console.log(`The multiplication of the above two digits are ${output}`)
+    }
+    else if (operator=="/"){
+        output = n1/n2;
+        console.log(`The division of the above two digits are ${output}`)
+    }
+
+
+
+
+
+
