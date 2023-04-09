3dc2b63a2e88b26d65afc7f63e034c82dba518b6
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-09 12:46:21 +0530

    readme3

--- DOM Projects/11_DOM Project/script.js
+++ DOM Projects/11_DOM Project/script.js
@@ -1,16 +1,17 @@
-const btn = document.createElement("button");
-btn.classList.add("btn");
-btn.innerText = "Clear Screen";
+const btn = document.createElement("button"); //create button
+btn.classList.add("btn"); //.classlist used to assign class to the button
+btn.innerText = "Clear Screen"; // label = clear screen
 document.body.appendChild(btn)
 
 document.body.addEventListener("click", (e) => {
-    const circle = document.createElement("div");
-    circle.style.left = `${e.clientX}px`;
-    circle.style.top = `${e.clientY}px`;
-    circle.classList.add("circle")
-    document.body.appendChild(circle)
+    const circle = document.createElement("div"); //creating a div 
+    circle.style.left = `${e.clientX}px`; //taking x axis values
+    circle.style.top = `${e.clientY}px`; // taking y axis values
+    circle.classList.add("circle") 
+    document.body.appendChild(circle) //appending circle
     })
 
+
 btn.addEventListener("mouseenter", () => {
-    location.reload();
+    location.reload(); // reload the current url
 })
