f3fcee9c5833c86c1654f5e7d94e990587b925fe
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-09 18:11:51 +0530

    first 5

---
+++ Js Assignment Q-A/Q 01/variable.js
@@ -0,0 +1,11 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+var a = "Ajay chauhan"
+var b = true
+var c = undefined
+var d = null 
+
+console.log(a + " and the datatype is " + typeof(a))
+console.log(b  + " and the datatype is " + typeof(b))
+console.log(c  + " and the datatype is " + typeof(c))
+console.log(d  + " and the datatype is " + typeof(d))
---
+++ Js Assignment Q-A/Q 02/interpolation.js
@@ -0,0 +1,8 @@
+//Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+const firstname = "Ajay"
+const lastname = "Chauhan"
+const maritalstatus = "unmarried"
+const country = "India"
+const age = 28
+
+console.log(`My first name is ${firstname} and last name is ${lastname} my country is ${country} age is ${age} and marital status is ${maritalstatus}`)
