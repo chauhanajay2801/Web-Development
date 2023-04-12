f5ce239c745d2d20b1576296bd809f5bc3203f52
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-12 11:26:57 +0530

    second assignment first task done

--- DOM Projects/01,02,03_DOM Project/about/about.js
+++ DOM Projects/01,02,03_DOM Project/about/about.js
@@ -1,3 +1,7 @@
+// task 1 make the headding color to grey
+let accordianHeadings = Array.from(document.querySelectorAll(".accordian > h3"));
+accordianHeadings.forEach(h3 => {
+    h3.style.backgroundColor = "#dadaf8";})
 let accordian = document.querySelectorAll(".accordian h3");
 accordian.forEach((element) => {
   element.addEventListener("click", () => {
@@ -9,3 +13,32 @@ accordian.forEach((element) => {
     }
   });
 });
+
+// task 2 add a same heading at the end with new content
+
+const accordianWrapper = document.querySelector(".accordian-wrapper");
+const div = document.createElement("div");
+div.classList.add("accordian")
+const fifthAccordian = accordianWrapper.appendChild(div);
+const skillHead = document.createElement("h3");
+skillHead.innerText = "Skills";
+const skills = fifthAccordian.appendChild(skillHead);
+const skillPara = document.createElement("p");
+skillPara.style.display = "block"
+skillPara.innerText = "I posses a very good command over the Full Stack Development technologies like MERN which can be seen in my work over GitHub";
+fifthAccordian.appendChild(skillPara)
+accordianHeadings = Array.from(document.querySelectorAll(".accordian > h3"));
+accordianHeadings.forEach(h3 => {
+    h3.style.backgroundColor = "#dadaf8";
+    let para = h3.nextElementSibling;
+    if (["Skills"].includes(h3.innerText)) {
+        para.style.display = "block";
+    } else {
+        para.style.display = "none";
+    }
+})
+
+
+
+
+
