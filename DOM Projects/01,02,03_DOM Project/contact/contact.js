b833a7e5da4587d749bb2a4b4bf2caed9e901be1
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-13 23:02:08 +0530

    Dom

---
+++ DOM Projects/01,02,03_DOM Project/contact/contact.js
@@ -0,0 +1,11 @@
+
+
+const userName = document.getElementById("check")
+// const userEmail = document.getElementByClass("userEmail")
+// const userMessage = document.getElementByClass("userMessage")
+// const enterName = document.getElementByClass("enterName")
+// const enterEmail = document.getElementByClass("enterMail")
+// const enterMessage = document.getElemenByClass("enterMessage")
+
+
+console.log(userName)
