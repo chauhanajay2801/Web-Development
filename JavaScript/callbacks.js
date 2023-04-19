ed98a651bbc49e8a7dfbb3c801202fee16bb45a6
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-19 13:55:32 +0530

    callbacks1

--- JavaScript/callbacks.js
+++ JavaScript/callbacks.js
@@ -1,3 +1,30 @@
+// Hof => Highr order functions 
+// Callback => function is calling a function
+
+// function a(){
+//   console.log("I was callled by function b");
+// }
+// function b(){
+//   a();
+// }
+
+// b();
+
+
+// const callback = (n) =>{
+//   return n**2;
+// };
+
+// function cube(callback,n){  // address/reference and the value
+//   return callback(n) * n;
+// }
+
+// console.log(cube(callback,2)); // n = 2 pass kia
+
+
+
+
+
 // function show(){
 //   console.log("I am show function");
 // }
@@ -40,27 +67,46 @@
 
 // myCalculator(10,20,ressult);
 
+//////////////////////////////////////Hofffffff//////////////////////////////////////////////////////////
+//hof callback
+
 // const arr = ["hey","ji","hello","namaste","ramram ji"];
 // arr.forEach((val) => {
   // console.log(val);
 // });
+//for each is hof and the ()=> inside it is a callback
 
-//SetTimeOut
+
+//@2nd method
+
+// arr.forEach(myfunc); //for each took the reference of arr and for each giving the refernce of other function 
+// the value in for each is the address and the myfunc with value is home
+//each time for each is called it call my func
+// function myfunc(val){
+  // console.log(val);
+// }
+
+
+
+////////////////////////////////////SetTimeOut/////////////////////////////////////////////////////////////
 
 // setTimeout(()=>{
-  // console.log("Hello this is the default timeout")},3000);
+//   console.log("Hello this is the default timeout")},3000);
 
 
-// function ajay(){
-  // console.log("new function")
+// function ajay(){     // uppar vale func ka breakdown ye hai
+//   console.log("new function")
 // }
 
 // setTimeout(ajay,4000);
 
 
-// setInterval
+////////////////////////////////////// setInterval///////////////////////////////////////////////////////////
+
 // setInterval(()=>{console.log("this is a set interval");},1000)
 
+
+/////////////////////////////callback types total////////////////////////////////////////////////////////////
 // how mamny callbacks do we have
 // foreach
 // map
@@ -111,7 +157,7 @@
 // let [var1, ,var2] = arr1
 // console.log(var1,var2)
 
-const arr1 = [1,2,3,4,5,6,7,8,9,11,12,13];
-let [var1,var2, ...res ] = arr1
-console.log(var1,var2)
-console.log(res)
+// const arr1 = [1,2,3,4,5,6,7,8,9,11,12,13];
+// let [var1,var2, ...res ] = arr1
+// console.log(var1,var2)
+// console.log(res)
