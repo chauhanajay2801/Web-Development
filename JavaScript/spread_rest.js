8ea19d5b66726fd9a9101846192f06d4de563359
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-19 15:45:16 +0530

    spread and rest

--- JavaScript/spread_rest.js
+++ JavaScript/spread_rest.js
@@ -1,25 +1,27 @@
-// spread and rest 
+///////////////////////////////// spread and rest////////////////////////////////////////////////////////////////// 
 // .... is called spread // sending the data 
 // .... is called rest // accumulating the data
 
-function someone(x,y,z,q,m) {
-    return x+y+z-q-m;
-}
+///////////////////////////////////////spread////////////////////////////////////////////////////////////////
 
-let vari = [5,7,5,4,2];
+// function someone(x,y,z,q,m) {
+//     return x+y+z-q-m;
+// }
 
-console.log(someone(...vari)); //spread and the data is divided it is called as spread
+// let vari = [5,7,5,4,2]; // right now in single variable
 
-
-// Rest
-
-const arr1 = [1,2,3,4,5,6,7,8,9,11,12,13];
-let [var1,var2, ...rs ] = arr1
-console.log(var1,var2)
-console.log(rs)
+// console.log(someone(...vari)); //spread and the data is divided it is called as spread
 
 
-function sum(...args){  //rest operator
+//////////////////////////////////////Rest///////////////////////////////////////////////////////////////////////////////////
+
+// const arr1 = [1,2,3,4,5,6,7,8,9,11,12,13];
+// let [var1,var2, ...rs ] = arr1
+// console.log(var1,var2)
+// console.log(rs)
+
+
+function sum(...args){  //rest operator that create the array
     console.log(args); // it is making the array and the spread is breaking the array
     let sum =0;
     for (const x of args){
