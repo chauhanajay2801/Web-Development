3a6313a39a86f83a3586adbbb46e49f6547e2b32
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-27 22:39:41 +0530

    dom 5.0

--- DOM Projects/05_DOM Project/05_DOM Project/script.js
+++ DOM Projects/05_DOM Project/05_DOM Project/script.js
@@ -1,12 +1,3 @@
-<a href="index.html" class="btn"> contact </a>
-.btn{
-    background-color: blueviolet;
-    padding: 10px 15px;
-    color: white;
-    border-radius: 3px;
-    margin: 10px;
-    font-size: 25px;
-}
-
-const div = document.createElement("div");
-div.classList.add = ("btn2");
+let element = document.getElementsByClassName("nav-center");
+element = element.childNodes;
+console.log(element);
