5ddf2184af96590c189cc7adccbc1c685eb7c12d
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-27 23:06:54 +0530

    dom 5.1

--- DOM Projects/05_DOM Project/05_DOM Project/script.js
+++ DOM Projects/05_DOM Project/05_DOM Project/script.js
@@ -1,3 +1,17 @@
-let element = document.getElementsByClassName("nav-center");
-element = element.childNodes;
-console.log(element);
+// //nav bar
+// const nav = document.querySelector(".");
+// console.log(nav)
+
+// let btn = document.createElement("a");
+// btn.classList.add("btn");
+// btn.textContent="Pro Subscription";
+
+
+const ab = document.querySelector(".nav-links")
+let x = document.createElement("a")
+x.classList.add("btn")
+x.textContent="Recipes 2"
+ab.append(x)
+ 
+
+
