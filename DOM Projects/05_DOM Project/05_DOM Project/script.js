d9a1d76601785229d11a51cb3a5b0d1417db1b46
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-28 00:20:54 +0530

    2 things completed

--- DOM Projects/05_DOM Project/05_DOM Project/script.js
+++ DOM Projects/05_DOM Project/05_DOM Project/script.js
@@ -1,11 +1,16 @@
 //Navbar
 const nav = document.getElementById("buttn")
-console.log(nav)
-
 let btn = document.createElement("a");
 btn.classList.add("btn");
 btn.textContent="Pro Subscription";
 nav.append(btn)
 
+// Adding chinese to the list
+let rec= document.querySelector(".tags-container > div");
+let a=document.createElement("a");
+a.innerText='Chinese (7)';
+rec.appendChild(a);
+
+// 
 
 
