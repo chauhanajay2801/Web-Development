2beece0f4283fa117fb9a7e09f9a11e3005c633a
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-27 23:42:12 +0530

    nav added

--- DOM Projects/05_DOM Project/05_DOM Project/script.js
+++ DOM Projects/05_DOM Project/05_DOM Project/script.js
@@ -1,18 +1,11 @@
-// //nav bar
-// const nav = document.querySelector(".");
-// console.log(nav)
+//Navbar
+const nav = document.getElementById("buttn")
+console.log(nav)
 
-// let btn = document.createElement("a");
-// btn.classList.add("btn");
-// btn.textContent="Pro Subscription";
+let btn = document.createElement("a");
+btn.classList.add("btn");
+btn.textContent="Pro Subscription";
+nav.append(btn)
 
-
-const ab = document.querySelector(".nav-links")
-console.log(ab)
-let x = document.createElement("a")
-x.classList.add("btn")
-x.textContent="Recipes 2"
-console.log(x)
- 
 
 
