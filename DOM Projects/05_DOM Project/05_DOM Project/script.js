d52c48d63d35f36cf31fcbe7478b3a4eee56342c
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-28 22:39:17 +0530

    5th project completed

--- DOM Projects/05_DOM Project/05_DOM Project/script.js
+++ DOM Projects/05_DOM Project/05_DOM Project/script.js
@@ -11,6 +11,38 @@ let a=document.createElement("a");
 a.innerText='Chinese (7)';
 rec.appendChild(a);
 
-// 
+
+let aa= document.querySelector(".recipe-gallery");     
+let ab= document.createElement("div");
+let ac=document.createElement("h4");
+ab.classList.add("card");
+ac.innerText='add 6th card here';
+ab.append(ac);
+aa.appendChild(ab);
+
+// adding the third dish
+// let a= document.querySelector(".recipe-gallery");
+// let dis = document.createElement("div");
+// let diss = document.createElement("h5");
+// let disss = document.createElement("p");
+// dis.classList.add("card");
+// diss.classList.add("recipe-name")
+// diss.innerText = ("Paneer Tikka")
+// disss.classList.add("recipe-disp")
+// disss.innerText = ("Prep : 30min | Cook: 30min")
+// console.log(dis)
+// console.log(diss)
+// console.log(disss)
 
 
+
+{/* <div class="card">
+                    <a href="#" class="recipe-text">
+                        <img src="./img/recipe-1.jpeg" class="recipe-img" />
+                        <h5 class="recipe-name">Chicken</h5>
+                        <p class="recipe-disp">Prep : 15min | Cook : 30min</p>
+                    </a>
+                </div> */}
+
+
+                
