d635713a577effa25148b6d5f5c8451533b0eec1
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-28 14:18:43 +0530

    to do add, remove , remove all completed

---
+++ React Projects/To do App/src/TodoList.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+function TodoList() {
+  const [activity, setactivity] = useState("")
+  const[listData, setlistData] = useState([])
+  function addTask(){
+    // setlistData([...listData,activity])
+    // console.log(listData)
+ 
+    setlistData((listData)=>{
+      const updatedList = [...listData, activity]
+      setactivity("")
+      return updatedList
+    })
+
+  }
+
+  function removeActivity(i){
+    const updatedListData = listData.filter((elem,id)=> {
+      return i!==id;
+    })
+    setlistData(updatedListData);
+
+  }
+
+  function removeAll(){
+    setlistData([])
+  }
+
+  return (
+    <>
+    <div>Todo List</div>
+    <input type="text" placeholder='Add Activity' value={activity} onChange={(e) => setactivity(e.target.value)}/>
+    <button onClick={addTask}>Add</button>
+    <p>All the tasks for today</p>
+    {listData!==[] && listData.map((data,i)=> {
+      return(
+        <>
+        <p key={i}></p>
+        <div>{data}</div>
+        <div><button onClick={()=>removeActivity(i)}>Remove(-)</button></div>
+        
+        </>
+      )
+    })}
+    {listData.length>=1 && 
+    <button onClick={removeAll}>Remove All Activity</button>}
+    </>
+  )
+}
+
+export default TodoList
