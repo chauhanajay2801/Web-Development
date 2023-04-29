ca5a547c4dbe58c6ed7f0bcc3177a8e597cb8cc3
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-29 17:08:17 +0530

    usestate increment counter

--- React/learning/src/App.js
+++ React/learning/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+
+  const[count, setCount] = useState(0);
+
+  const decrement = () =>{
+    setCount(count-1)
+  }
+
+  const increment = () =>{
+      setCount(count+1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>counter App</h1>
+    <div>{count}</div>
+    <div>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
-  );
+    </div>)
+  
 }
 
 export default App;
