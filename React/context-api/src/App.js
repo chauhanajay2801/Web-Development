690748e5637e62e90b1ba83195004e42021a073b
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-29 23:34:29 +0530

    contextapi 0

--- React/context-api/src/App.js
+++ React/context-api/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
+
+  const [name, setname] = useState("");
+  const [color, setColor] = useState("");
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <h1>Hey</h1>
+  <Navbar/>
+  <form>
+    <input type={"text"} placeholder='Enter UserName' value={name} onChange={myname}/>
+    <input type={"text"} placeholder='Enter a colorName' value={color} onChange={myColor}/>
+  </form>
+  </>
   );
 }
 
