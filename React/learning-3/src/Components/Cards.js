8c0798fa2f1b185f1f48e541e1f2df80457cba66
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-04-29 14:30:05 +0530

    props

---
+++ React/learning-3/src/Components/Cards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cards(props) {
+  return (
+    <>
+   <h1>Cards,{props.name}</h1>
+    </>
+  )
+}
+
+export default Cards;
---
+++ React/learning-3/src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import Cards from "./Cards";
+
+function App() {
+  return (
+    <>
+     <h1 className="text-green-700 font-bold">Hello There</h1>
+     <Cards name = "Ajay Chauhan"/>
+     <Cards name = "Shiv "/>
+     <Cards name = "Jd Bose"/>
+     <Cards name = "Rohit Biswas"/>
+    </>
+  );
+}
+
+export default App;
