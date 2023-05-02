8c3647f80f4f966ab6eae99c57d3c239a9a9ffc5
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-02 15:51:22 +0530

    local storage added

--- React Projects/contacts-app/src/components/App.js
+++ React Projects/contacts-app/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
@@ -8,12 +8,17 @@ import ContactList from './ContactList';
 
 function App() {
 
+const LOCAL_STORAGE_KEY = "contacts"; // key to provide in the use effect for local storage need
 const [contacts, setContacts] = useState([])
 
 const addContactHandler = (contact) =>{
   console.log(contact)
-  setContacts([...contacts, contact])
-}
+  setContacts([...contacts, contact]) //array destructuring
+};
+
+useEffect(()=>{
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts)); //adding key
+},[contacts]) //adding contacts as dependency
 
 
   return (
