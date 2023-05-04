44d5923be04f244979a2521b495b6f60369a471f
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-04 23:59:07 +0530

    footer added

--- React Projects/first/src/App.js
+++ React Projects/first/src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
+import Footer from './components/Footer'
 import "./styles/App.scss"
 import "./styles/header.scss"
 import "./styles/home.scss"
+import "./styles/footer.scss"
 
 const App = () => {
   return (
@@ -13,6 +15,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
+      <Footer/>
     </Router>
   )
 }
