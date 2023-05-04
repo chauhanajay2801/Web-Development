b03ce54e3bdc68cc3afdf9f5498285abb2309b98
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-04 16:34:05 +0530

    added new

---
+++ React Projects/first/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
