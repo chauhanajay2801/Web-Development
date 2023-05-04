338f64971bac90d986d0fc74f67b7003979495e9
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-04 15:12:00 +0530

    routing link done

--- React Projects/my-app/src/components/Header.jsx
+++ React Projects/my-app/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
+
+
+// to connect the react router dom we will import the Link tag
 const Header = () => {
   return (
     <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
     </nav>
   )
 }
