bb2304c48a79156c256489bbad0dac7c1c542edb
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-04 15:26:47 +0530

    done

---
+++ React Projects/learning routes and export/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+// to connect the react router dom we will import the Link tag
+const Header = () => {
+  return (
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+    </nav>
+  )
+}
+
+export default Header
