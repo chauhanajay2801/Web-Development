6a5be715564983d1e7eb7b85a1cda2d072555b71
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-05 22:10:48 +0530

    services component added

--- React Projects/first/src/App.js
+++ React Projects/first/src/App.js
@@ -4,11 +4,13 @@ import Header from './components/Header'
 import Home from './components/Home'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
+import Services from './components/Services'
 import "./styles/App.scss"
 import "./styles/header.scss"
 import "./styles/home.scss"
 import "./styles/footer.scss"
 import "./styles/contact.scss"
+import "./styles/services.scss"
 
 const App = () => {
   return (
@@ -17,6 +19,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/contact' element={<Contact/>}/>
+        <Route path='/services' element={<Services/>}/>
       </Routes>
       <Footer/>
     </Router>
