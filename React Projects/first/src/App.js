87ada148a74238db3640b3553a1ff9be49e0f45d
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-05 22:38:08 +0530

    media for tab

--- React Projects/first/src/App.js
+++ React Projects/first/src/App.js
@@ -11,6 +11,7 @@ import "./styles/header.scss"
 import "./styles/home.scss"
 import "./styles/footer.scss"
 import "./styles/contact.scss"
+import "./styles/mediaqueries.scss"
 
 
 const App = () => {
