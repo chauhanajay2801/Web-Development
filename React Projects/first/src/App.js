04fe2459993827f444fba349d071e5b663b40f41
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-05 16:40:23 +0530

    contact component added

--- React Projects/first/src/App.js
+++ React Projects/first/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
 import Footer from './components/Footer'
+import Contact from './components/Contact'
 import "./styles/App.scss"
 import "./styles/header.scss"
 import "./styles/home.scss"
 import "./styles/footer.scss"
+import "./styles/contact.scss"
 
 const App = () => {
   return (
@@ -14,6 +16,7 @@ const App = () => {
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
       </Routes>
       <Footer/>
     </Router>
