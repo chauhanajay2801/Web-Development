4e64cdf57d519ac15cf31b48e23fe4ac4a62cf6c
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-06 10:12:42 +0530

    structure defined

---
+++ React Projects/newapp/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+
+function App() {
+ return
+ <Router>
+  
+  <Routes>
+    <Route/>
+  </Routes>
+
+ </Router>
+
+  
+}
+
+export default App;
