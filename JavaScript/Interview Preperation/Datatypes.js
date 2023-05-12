d09ae3f3d30a921800e437a9f0351a84590bb79f
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-12 12:29:15 +0530

    Datatypes 1

---
+++ JavaScript/Interview Preperation/Datatypes.js
@@ -0,0 +1,20 @@
+// Example of dynamic language 
+// let a = 21;
+// console.log(a);
+// console.log(typeof(a));
+// a = "rikshaw";
+// console.log(typeof(a));
+// a = true;
+// console.log(typeof(a));
+
+// Weakly typed -- allow implicit type conversion when an operation involves mismatched types, instead of throwing exception
+
+// const a = 21;
+// const b = a + "1"
+// console.log(typeof(b));
+// console.log(b);
+
+
+
+
+
