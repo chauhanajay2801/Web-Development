59f7c89f47458da68f1940eece6f3788cbfd0664
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-14 11:47:22 +0530

    form and route added

--- React/hookkss/src/App.js
+++ React/hookkss/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import "./styles/App.css";
+import "./styles/Header.css";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
 const App = () => {
   return (
     <Router>
       <Header/>
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<div>Page not found 404</div>} />
       </Routes>
     </Router>
   );
