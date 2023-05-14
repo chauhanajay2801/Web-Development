334162f5c78d9ccb7aa954f158dbd28a3e751f34
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-14 13:15:24 +0530

    use effect1

--- React/hookkss/src/components/Home.jsx
+++ React/hookkss/src/components/Home.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import Coin from './Product';
-
+import React, { useEffect, useState } from "react";
+import Coin from "./Product";
 
 const Home = () => {
+  const arr = [1, 2, 3, 4, 5];
 
-const arr = [1,2,3,4,5];
+const [temp, setTemp] = useState(0);
+
+  useEffect(()=>{
+    console.log('Working');
+  },[temp])
 
   return (
     <div>
-        {
-            arr.map(i => (
-                <Coin name = {"Bitcoin"}  symbol={"Btc"} key={i}/>
-            ))
-        }
+      <button onClick={() => setTemp(temp+1)}>Press me</button>
+      {arr.map((i) => (
+        <Coin name={"Bitcoin"} symbol={"Btc"} key={i} />
+      ))}
+      <h1>{temp}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
