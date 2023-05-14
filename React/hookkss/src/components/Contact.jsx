a84a48cfffd8aae1780c5fc00d8dfb6c161e1569
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-14 11:41:56 +0530

    ew project for learning hooks

---
+++ React/hookkss/src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="email" />
+      </form>
+    </div>
+  )
+}
+
+export default Contact
---
+++ React/hookkss/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Product from './Product';
+
+
+const Home = () => {
+
+const arr = [1,2,3,4,5];
+
+  return (
+    <div>
+        {
+            arr.map(i => (
+                <Product value = {i} key={i}/>
+            ))
+        }
+    </div>
+  )
+}
+
+export default Home
