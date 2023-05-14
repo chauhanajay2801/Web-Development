d4229ce1475f07dc039c7723cba3de252bc27034
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-14 11:16:18 +0530

    v1

--- React/hooks-learning/src/App.js
+++ React/hooks-learning/src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
-import HookCounterFour from './components/HookCounterFour';
+import React from "react";
+import "./styles/App.css";
+import "./styles/Header.css";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <HookCounterFour/>
-    </div>
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<div>Page not found 404</div>} />
+      </Routes>
+    </Router>
   );
 }
 
