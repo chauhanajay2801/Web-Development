d6eac11c1b05dc6f2004c6c99ab114cb6a74a84f
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-14 12:32:55 +0530

    use state done

--- React/hookkss/src/components/Contact.jsx
+++ React/hookkss/src/components/Contact.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [name, setName] = useState("Initial");
+
   return (
     <div>
       <form>
-        <input type="text" />
-        <input type="email" />
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input type="email" placeholder="Email" />
       </form>
+      <button onClick={() => console.log(name)}>pressed</button>
+      <p>{name}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
