081a5719c585c3f5d626b29da123eddb2fa4c78a
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-05-14 15:30:42 +0530

    axios

--- React/hookkss/src/components/Home.jsx
+++ React/hookkss/src/components/Home.jsx
@@ -1,22 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect} from "react";
 import Coin from "./Product";
+import axios from "axios";
 
 const Home = () => {
   const arr = [1, 2, 3, 4, 5];
 
-const [temp, setTemp] = useState(0);
+
 
   useEffect(()=>{
-    console.log('Working');
-  },[temp])
+
+    const fetchAllCoins = async () => {
+      const { data } = await axios.get('https://api.coingecko/api/v3/coins/markets?vs_currency=inr&per_page=20');
+
+    console.log(data)
+    };
+
+    fetchAllCoins();
+  },[])
 
   return (
     <div>
-      <button onClick={() => setTemp(temp+1)}>Press me</button>
       {arr.map((i) => (
         <Coin name={"Bitcoin"} symbol={"Btc"} key={i} />
       ))}
-      <h1>{temp}</h1>
     </div>
   );
 };
